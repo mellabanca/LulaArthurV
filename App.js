c5cd1abeb386af0921dca6ceb35a5e13b07232df
mellabanca <mellabanca@gmail.com>
2022-12-09 14:47:15 -0300

    Aula 76 o Brasil perdeu

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/Home';
+import MeteorScreen from './screens/Meteor';
+import IssLocationScreen from './screens/IssLocation';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Casa"
+                       screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Casa" component={HomeScreen}/>
+        <Stack.Screen name="UbicaciónDeLaNasa" component={IssLocationScreen}/>
+        <Stack.Screen name="Meteorito" component={MeteorScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
