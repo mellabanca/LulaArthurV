db2ff8db1b364295a3f0e49b9affbf09db7262af
mellabanca <mellabanca@gmail.com>
2022-12-28 15:26:27 -0300

    Aula 79 amenaza meteoritos

--- screens/Meteor.js
+++ screens/Meteor.js
@@ -2,7 +2,48 @@ import React, {Component} from "react";
 import {Text, View} from "react-native";
 
 export default class MeteorScreen extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            meteoritos: {}
+        }
+    }
+
+    componentDidMount(){
+        this.getMeteorito();
+    }
+
+    getMeteorito = () => {
+        axios.get("https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=BohhdOzumYH9K75Y31iNdUUBvHjvwuDhAui2R5wQ")
+             .then(response=>{
+                this.setState({
+                    meteoritos: response.data.near_earth_objects
+                })
+             })
+             .catch(error=>{
+                Alert.alert(error.message)
+             })
+    }
+
     render(){
+        if(Object.keys(this.state.meteoritos).length === 0){
+            return(
+                <View style={styles.loadingLocation}>
+                    <Text>Cargando...</Text>
+                </View>
+            )
+        } else {
+            let meteoritoArr = Object.keys(this.state.meteoritos).map(dadosMeteoritos => {
+                return this.state.meteoritos[dadosMeteoritos]
+            })
+            let meteoritos = [].concat.apply([], meteoritoArr);
+            meteoritos.forEach(function(elemento){
+                let diametro = (elemento.estimated_diameter.kilometers.estimated_diameter_min +
+                                elemento.estimated_diameter.kilometers.estimated_diameter_max)/2;
+                let amenaza = (diametro/elemento.close_approach_data[0].miss_distance.kilometers)
+                              *1000000000;
+                elemento.amenazaF = amenaza;
+            })
         return(
             <View style = {{
                 flex: 1,
@@ -12,5 +53,13 @@ export default class MeteorScreen extends Component{
                 <Text>Meteorito.</Text>
             </View>
         )
-    }
+    }}
 }
+
+const styles = StyleSheet.create({
+    loadingLocation: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+})
